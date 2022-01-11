a83e73e4c94ae6a619af6e109bd96eb4f4318fcd
victorlincoln10 <victorlincoln10@gmail.com>
2022-01-11 10:11:01 -0400

    alterações 2

--- aulas/aulas/Exercicio1/scriptaulas.js
+++ aulas/aulas/Exercicio1/scriptaulas.js
@@ -1,17 +1,20 @@
 function carregar(){
-    var msg1 = document.getElementById('msg')
-    var img1 = document.getElementById('img')
+    let msg = document.getElementById('msg')
+    var img = document.getElementById('img') 
     var data = new Date()
-    var hora = 21
-    //var hora = data.getHours()
-    msg1.innerHTML = `Agora sao ${hora} horas`
+    //var hora = 21
+    var hora = data.getHours()
+    msg.innerHTML = `Agora sao ${hora} horas`
     if (hora >=0 && hora <12){
         //Bom dia!
-        img1.src = 'manha.jpg'
+        img.src = 'manha.jpg'
+        document.body.style.background = '#e2cd9f'
     }else if(hora>= 12 && hora <= 18){
         //Boa tarde
-        img1.src ='tarde.jpg'
+        img.src ='tarde.jpg'
+        document.body.style.background = '#b9846f'
     }else{  
-        img1.src = 'noite.jpg'
+        img.src = 'noite.jpg'
+        document.body.style.background = '#515154'
     }
 }
